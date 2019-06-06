f14216cab50de681e3b09c6b1422f20ebb989e80
Orlando J Betancourth <orlando.betancourth@gmail.com>
2019-06-06 09:07:48 -0600

    Put y Delete
    Added Put and Delete route entries

--- obtBackEnd/routes/api/things.js
+++ obtBackEnd/routes/api/things.js
@@ -17,7 +17,7 @@ var thingsStruct = {
 thingsCollection.push(
   Object.assign({},
       thingsStruct,
-      { "id":"1",
+      { "id":1,
         "descripcion":"Mi Primer Thing",
         "fecha": new Date().getTime(),
         "by":"Orlando"
@@ -50,13 +50,29 @@ router.post('/', (req, res, next)=>{
   thingsCollection.push(newElement);
   res.status(200).json(newElement);
 }); // post /
-
-router.put('/', (req, res, next) => {
-  res.status(200).json({ 'msg': 'Entro en el put' });
+// http://localhost:3000/api/things/1236183491
+router.put('/:idElemento', (req, res, next) => {
+  var id = parseInt(req.params.idElemento);
+  var update = req.body;
+  var modifiedObject = {};
+  var originalObject = {};
+  thingsCollection = thingsCollection.map( (e, i) => {
+    if(e.id === id) {
+      originalObject = Object.assign({},e);
+      return Object.assign(modifiedObject, e, req.body);
+    }
+    return e;
+  } );//map
+  res.status(200).json({"o":originalObject, "m": modifiedObject });
 });// put /
-
-router.delete('/', (req, res, next) => {
-  res.status(200).json({ 'msg': 'Entro en el delete' });
+// router.delete('/:id/:soft', (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
+  var id = parseInt(req.params.id);
+  //var soft = req.params.soft;
+  thingsCollection = thingsCollection.filter( (e, i) => {
+    return (e.id !== id );
+  } ); //
+  res.status(200).json({ 'msg': 'Elemento ' + id + ' fué eleminido!!!' });
 });// put /
 
 module.exports = router;
