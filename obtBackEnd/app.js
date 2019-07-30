a61906d8d652ddc83ff1a8a76c4d6ff902d796c0
Orlando J Betancourth <orlando.betancourth@gmail.com>
2019-07-29 19:09:28 -0600

    Preparando Para Producción en One Big Thing
    1) Se agrega cpx para poder copiar el build del frontend a la carpeta public del backend.
    2) Se modifica app.js  para correr el aplicativo frontend 
    3) Se modifica bin/www para seleccionar DB de producción o DB de desarrollo

--- obtBackEnd/app.js
+++ obtBackEnd/app.js
@@ -25,9 +25,13 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+
+app.get('/*', (req, res, next)=>{
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ obtBackEnd/public/precache-manifest.b2b2d4e91e1c9d6d5239e1cabacff8d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334d9167b4e39233768dc90cb1b68a90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a59642ece2c809318ed",
+    "url": "/static/css/main.c81dcf83.chunk.css"
+  },
+  {
+    "revision": "84fdb01fa6960dacd1bd",
+    "url": "/static/js/2.397eea85.chunk.js"
+  },
+  {
+    "revision": "0a59642ece2c809318ed",
+    "url": "/static/js/main.d3112449.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
